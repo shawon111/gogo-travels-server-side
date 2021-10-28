eebb99a614e40f16d62fc71a79145b6ce9d4fa87
shawon ahmmed <shawonahmmed08509@gmail.com>
2021-10-29 02:41:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('travel agency website server')
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gv57l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db('travel_mafia');
+      const packageCollection = database.collection('packages');
+      
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log('listening to the port: ', port)
+})
